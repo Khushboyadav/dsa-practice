28438f37e8d0fb568dcd4d741b34d264341651bf
khushboo yadav <142902822+Khushboyadav@users.noreply.github.com>
2026-07-19 21:38:30 +0530

    Ceiling Number and Floor Number in the array

---
+++ Arrays/FloorElement.java
@@ -0,0 +1,32 @@
+public class FloorElement {
+    public static void main(String args[]) 
+    {
+        int[] arr = { 2,3,5,9,14,16,18 };
+        int item = 4;
+        int ans=binarysearch(arr, item);
+        System.out.println("Floor Element "+arr[ans]);
+    }
+
+    static int binarysearch(int[] arr, int item) 
+    {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) 
+        {
+            int mid = start + (end - start) / 2;
+            if (item < arr[mid]) 
+            {
+                end = mid - 1;
+            } 
+            else if (item > arr[mid]) 
+            {
+                start = mid + 1;
+            } 
+            else 
+            {
+                return mid;
+            }
+        }
+        return end;
+    }
+}
